26c6b58b04912da83bbd166860fa413fc265cf21
Aditya <adityarepe433@gmail.com>
2023-10-21 00:24:00 +0530

    Aditya added project files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ChakraProvider theme={theme}>
     <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
@@ -15,3 +18,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+export const server = 'https://api.coingecko.com/api/v3'
